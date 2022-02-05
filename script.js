eb141760922ff27018263413ae98c2ea9c425f14
RufixDuke <rufixduke01@gmail.com>
2022-02-05 03:03:35 +0100

    Second Commit

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const btn = document.querySelector('.btn');
 const inputs = document.getElementById('inputs');
 var displaySpace = document.getElementById('display-space')
-
+var speed = document.getElementById('videos');
 
 btn.addEventListener('click', () => {
     var query = inputs.value;
     weatherFetch(query)
 })
+
+speed.playbackRate = 0.4;
 
 function weatherFetch(query) {
     fetch(`https://api.openweathermap.org/data/2.5/weather?id=${query}&appid=de91c982f0db3cc64f7ae493e2a70b93`)
@@ -14,20 +16,19 @@ function weatherFetch(query) {
             return result.json();
         })
         .then(res => {
-            console.log(res.main)
+            console.log(res)
 
             displaySpace.innerHTML = '';
 
 
             var space = `<div>
                                 <h2>Details</h2>
-                                <p>Temp: ${res.main.temp}</p>
-                                <p>Temp Max: ${res.main.temp_max}</p>
-                                <p>Temp Min: ${res.main.temp_min}</p>
+                                <p>Temp: ${res.main.temp}&deg;C</p>
+                                <p>Temp Max: ${res.main.temp_max}&deg;C</p>
+                                <p>Temp Min: ${res.main.temp_min}&deg;C</p>
                                 <p>Humidity: ${res.main.humidity}</p>
                                 <p>Pressure: ${res.main.pressure}</p>
                                 <p>Wind: ${res.wind.speed}</p>
-                                <p>Weather: ${res.weather.main}</p>
                             </div>`;
 
 
